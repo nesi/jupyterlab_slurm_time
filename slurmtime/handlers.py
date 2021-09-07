d099cd5e9ab0e674d2146031d90e36e910925dec
Chris Scott <chris@chrisdjscott.co.uk>
2021-09-07 15:26:45 +1200

    getting timeleft from squeue

--- slurmtime/handlers.py
+++ slurmtime/handlers.py
@@ -1,8 +1,11 @@
+import os
 import json
+import subprocess
 
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
+
 
 class RouteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
@@ -10,8 +13,16 @@ class RouteHandler(APIHandler):
     # Jupyter server
     @tornado.web.authenticated
     def get(self):
+        jobid = os.environ.get("SLURM_JOB_ID")
+        data = None
+        if jobid is not None:
+            result = subprocess.run(["squeue", "--noheader", "-O", "TimeLeft", "-j", jobid],
+                                    capture_output=True, text=True)
+            if result.returncode == 0:
+                data = result.stdout.strip()
+
         self.finish(json.dumps({
-            "data": "This is /slurmtime/get_example endpoint!"
+            "data": data,
         }))
 
 
@@ -19,6 +30,6 @@ def setup_handlers(web_app):
     host_pattern = ".*$"
 
     base_url = web_app.settings["base_url"]
-    route_pattern = url_path_join(base_url, "slurmtime", "get_example")
+    route_pattern = url_path_join(base_url, "slurmtime", "get_time_remaining")
     handlers = [(route_pattern, RouteHandler)]
     web_app.add_handlers(host_pattern, handlers)
--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,86 @@ import {
 
 import { requestAPI } from './handler';
 
-/**
- * Initialization data for the slurmtime extension.
- */
-const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'slurmtime:plugin',
-  autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension slurmtime is activated!');
+import { Widget } from '@lumino/widgets';
 
-    requestAPI<any>('get_example')
+import { Poll } from '@lumino/polling';
+
+import { ITopBar } from 'jupyterlab-topbar';
+
+/**
+ * Refresh interval for polling the time remaining
+ */
+const REFRESH_INTERVAL = 5000;
+
+/**
+ * A widget for displaying the time remaining
+ */
+class SlurmTimeRemainingWidget extends Widget {
+  /**
+   * Create a new time remaining widget
+   */
+
+  constructor() {
+    super();
+
+    // create the text element
+    this.addClass('slurmtime-widget');
+    this.textNode = document.createElement('div');
+    this.textNode.textContent = 'N/A';
+    this.node.appendChild(this.textNode);
+
+    // do an initial update of the time remaining
+    //void this._updateTimeRemaining();
+    // also update periodically
+    this._poll = new Poll({
+      factory: async () => {
+        await this._updateTimeRemaining();
+      },
+      frequency: { interval: REFRESH_INTERVAL }
+    });
+  }
+
+  dispose(): void {
+    super.dispose();
+    this._poll.dispose();
+  }
+
+  async _updateTimeRemaining(): Promise<void> {
+    console.log('Slurm time update requested');
+
+    requestAPI<any>('get_time_remaining')
       .then(data => {
-        console.log(data);
+        const time_text = data['data'];
+        if (time_text !== null) {
+          this.textNode.textContent = 'Time remaining: ' + data['data'];
+        } else {
+          this.textNode.textContent = '';
+        }
       })
       .catch(reason => {
         console.error(
           `The slurmtime server extension appears to be missing.\n${reason}`
         );
       });
+  }
+
+  readonly textNode: HTMLDivElement;
+
+  private _poll: Poll;
+}
+
+/**
+ * Initialization data for the slurmtime extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'slurmtime:plugin',
+  autoStart: true,
+  requires: [ITopBar],
+  activate: (app: JupyterFrontEnd, topBar: ITopBar) => {
+    console.log('JupyterLab extension slurmtime is activated!');
+
+    const timeWidget = new SlurmTimeRemainingWidget();
+    topBar.addItem('slurm-time-remaining', timeWidget);
   }
 };
 
